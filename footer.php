e4adb10ffe2e1a743424955a022ab0eac8388093
Pablo Sanchez <psanchezdev345@gmail.com>
2023-08-11 19:12:46 -0600

    added nav tags in footer menu

--- footer.php
+++ footer.php
@@ -5,13 +5,15 @@
   <div class="footer-main">
     <div class="copyright">Copyright 2023 - Tarrega - All Rights Reserved.</div>
     <div class="footer-menu">
-      <?php
-        wp_nav_menu(
-          array(
-            'theme_location' => 'tarrega_footer_menu'
-          )
-        );
-      ?>
+      <nav>
+        <?php
+          wp_nav_menu(
+            array(
+              'theme_location' => 'tarrega_footer_menu'
+            )
+          );
+        ?>
+      </nav>
     </div>
   </div>
 </footer>
