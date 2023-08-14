de34d55813ee1016061cae2f04135938f260da82
Pablo Sanchez <psanchezdev345@gmail.com>
2023-08-14 17:25:35 -0600

    added error 404 page

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<main>
+  <div class="container e404">
+    <h1>Page not found</h1>
+    <p>Unfortunately, the page you tried to reach does not exist.</p>
+
+    <!-- Add other content here -->
+
+  </div>
+</main>
+
+<?php get_footer(); ?>
