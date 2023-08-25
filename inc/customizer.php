f4eeac6f2233dd17146f15977aa75efa4fd6596d
Pablo Sanchez <psanchezdev345@gmail.com>
2023-08-25 11:51:27 -0600

    started header customizer settings

--- inc/customizer.php
+++ inc/customizer.php
@@ -28,6 +28,33 @@ $wp_customize->add_control(
     'type' => 'text'
   )
 );
+
+//Fixed header option
+$wp_customize->add_section(
+  'sec_header',
+  array(
+    'title' => 'Header',
+    'description' => 'Customize the settings for your header'
+  )
+);
+
+$wp_customize->add_setting(
+  'set_sticky_header',
+  array(
+    'type' => 'theme_mod',
+  )
+);
+
+$wp_customize->add_control(
+  'set_sticky_header',
+  array(
+    'label' => 'Stick to top',
+    'description' => 'Fix the main header to the top of the page',
+    'section' => 'sec_header',
+    'type' => 'checkbox'
+  )
+);
+
 }
 
 add_action( 'customize_register', 'tarrega_customizer' );
